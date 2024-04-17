f4ee93b271ba8f718e250cda8b9e12df18fa3c27
Brianca Knight <briancaknight@gmail.com>
2024-04-17 09:25:17 -0700

    Initial Commit

---
+++ MOOD/MOODApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOODApp.swift
+//  MOOD
+//
+//  Created by Brianca Knight on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MOODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
